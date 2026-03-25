2f1d9368a63856c4efb3261337ae5b82509e5517
CiciTang090905 <wtang28@jh.edu>
2026-03-25 17:22:33 -0400

    dashboard-07: Connect components with props and lift state up

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import SelectCountry from "@/components/ui/select-country";
 import DisplayStatistics from "@/components/ui/display-statistics";
+import { useState } from "react"; // 👀
 
 function App() {
+  const [countryCode, setCountryCode] = useState("US"); // 👀 Default to the US
+
   return (
     <div className="flex flex-col justify-between items-center min-h-screen max-w-4xl m-auto py-10">
-      <SelectCountry />
-      <DisplayStatistics /> 
+      //lift up
+      <SelectCountry setCountryCode={setCountryCode} />  
+      <DisplayStatistics countryCode={countryCode} />
     </div>
   );
 }
+
+// lifting state up pattern — move the shared state to their common parent, App. 
+// The App component will own the countryCode state and pass it down to both children.
 
 export default App;
