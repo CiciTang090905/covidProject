b9f6b8c163e88152a00992d9f29e1b6bbe6e4977
CiciTang090905 <wtang28@jh.edu>
2026-03-25 16:02:22 -0400

    dashboard-02: Define types and API function for disease.sh

---
+++ src/services/disease.ts
@@ -0,0 +1,26 @@
+import type {
+    CovidData,
+    DiseaseApiResponse,
+    DiseaseCountryInfo,
+} from "@/types/covid";
+
+const REST_DISEASE_API = "https://disease.sh/v3/covid-19/countries";
+
+export async function fetchCovidData(countryCode: string): Promise<CovidData> {
+  const response = await fetch(`${REST_DISEASE_API}/${countryCode}`);
+  if (!response.ok) {
+    throw new Error(`API request failed! with status: ${response.status}`);
+  }
+  const data: DiseaseApiResponse = await response.json();
+  const countryInfo: DiseaseCountryInfo = data.countryInfo;
+
+  // Parse and return the required data
+  return {
+    countryName: data.country,
+    countryCode: countryInfo.iso2,
+    countryFlag: countryInfo.flag,
+    confirmed: data.cases,
+    active: data.active,
+    recovered: data.recovered,
+  };
+}
